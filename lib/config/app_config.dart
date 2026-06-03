589b1a9c54365431f220965c2a2a56c3bce96e65
tria <triatria329@gmail.com>
2026-06-03 15:22:35 +0700

    Update konfigurasi, fitur login email, dan perbaikan UI login
    - Mengubah IP server ke 10.0.2.2 untuk pengujian di emulator Android.
    
    - Menambahkan fitur registrasi dan login menggunakan Email/Password pada AuthProvider.
    
    - Mempergelap warna teks placeholder dan garis pembatas pada layar login agar lebih mudah dibaca.

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -4,7 +4,7 @@ class AppConfig {
   // CRITICAL: Change this IP address to match your local machine's IP (e.g., 192.168.1.X) when testing on a physical device.
   // "10.0.2.2" is the special loopback address to access the host machine's localhost from the Android Emulator.
   // "127.0.0.1" is standard for iOS Simulator or Web.
-  static const String serverIp = "192.168.1.15";
+  static const String serverIp = "10.0.2.2";
   //ubah menjadi 192.168.1.15 jika run di HP fisik (OPPO)
   //ubah menjadi 10.0.2.2 jika run di Android Emulator
   //ubah menjadi localhost jika run di computer (web/desktop)
