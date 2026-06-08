2bbb932fc90cffeeea32436a21ddab79d09ec276
Rohayati <ryati7239@gmail.com>
2026-06-08 23:26:19 +0700

    Deploy web app with unique turf images

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2808729638" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1235943437" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
